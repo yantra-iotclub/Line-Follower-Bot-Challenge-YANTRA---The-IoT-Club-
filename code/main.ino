fe18bc8f9736b9e5cdc38c785407ffba86443efc
🚀 YANTRA - The IoT Club <yantratheiotclub@gmail.com>
2025-03-08 22:09:26 +0530

    Update main.ino

--- code/main.ino
+++ code/main.ino
@@ -1,30 +1,36 @@
 // main.ino - Line Follower Bot Challenge | YANTRA - The IoT Club
-// Main control logic for the line follower bot
+// This file contains the main logic for the line follower bot.
 
 #include "sensor_config.ino"
 #include "motor_control.ino"
 
-#define IR_SENSOR_RIGHT 11
-#define IR_SENSOR_LEFT 12
-#define MOTOR_SPEED 50
-
 void setup() {
+    // Configure sensors and motors
     configureSensors();
     configureMotors();
+    
+    // Stop motors initially
     stopMotors();
 }
 
 void loop() {
-    int rightIRSensorValue = digitalRead(IR_SENSOR_RIGHT);
-    int leftIRSensorValue = digitalRead(IR_SENSOR_LEFT);
+    int rightIRSensorValue = readRightSensor();
+    int leftIRSensorValue = readLeftSensor();
 
+    // If none of the sensors detect a black line, move forward
     if (rightIRSensorValue == LOW && leftIRSensorValue == LOW) {
-        rotateMotors(MOTOR_SPEED, MOTOR_SPEED); // Move forward
-    } else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW) {
-        rotateMotors(-MOTOR_SPEED, MOTOR_SPEED); // Turn right
-    } else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH) {
-        rotateMotors(MOTOR_SPEED, -MOTOR_SPEED); // Turn left
-    } else {
-        stopMotors(); // Stop when both sensors detect black
+        rotateMotors(MOTOR_SPEED, MOTOR_SPEED);
+    }
+    // If right sensor detects a black line, turn right
+    else if (rightIRSensorValue == HIGH && leftIRSensorValue == LOW) {
+        rotateMotors(-MOTOR_SPEED, MOTOR_SPEED);
+    }
+    // If left sensor detects a black line, turn left
+    else if (rightIRSensorValue == LOW && leftIRSensorValue == HIGH) {
+        rotateMotors(MOTOR_SPEED, -MOTOR_SPEED);
+    }
+    // If both sensors detect a black line, stop
+    else {
+        stopMotors();
     }
 }
