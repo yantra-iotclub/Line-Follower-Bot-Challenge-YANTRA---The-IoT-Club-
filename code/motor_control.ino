e5d86100dea7597164b97f28577fb57f3deac879
🚀 YANTRA - The IoT Club <yantratheiotclub@gmail.com>
2025-03-08 22:08:47 +0530

    Update motor_control.ino

--- code/motor_control.ino
+++ code/motor_control.ino
@@ -1,1 +1,59 @@
+// motor_control.ino - Line Follower Bot Challenge | YANTRA - The IoT Club
+// This file handles motor configuration and control.
+
+#define MOTOR_SPEED 50
+
+// Right motor
+int enableRightMotor = 6;
+int rightMotorPin1 = 7;
+int rightMotorPin2 = 8;
+
+// Left motor
+int enableLeftMotor = 5;
+int leftMotorPin1 = 9;
+int leftMotorPin2 = 10;
+
+void configureMotors() {
+    pinMode(enableRightMotor, OUTPUT);
+    pinMode(rightMotorPin1, OUTPUT);
+    pinMode(rightMotorPin2, OUTPUT);
+    
+    pinMode(enableLeftMotor, OUTPUT);
+    pinMode(leftMotorPin1, OUTPUT);
+    pinMode(leftMotorPin2, OUTPUT);
+}
+
+void rotateMotors(int rightMotorSpeed, int leftMotorSpeed) {
+    // Right motor direction control
+    if (rightMotorSpeed < 0) {
+        digitalWrite(rightMotorPin1, LOW);
+        digitalWrite(rightMotorPin2, HIGH);
+    } else if (rightMotorSpeed > 0) {
+        digitalWrite(rightMotorPin1, HIGH);
+        digitalWrite(rightMotorPin2, LOW);
+    } else {
+        digitalWrite(rightMotorPin1, LOW);
+        digitalWrite(rightMotorPin2, LOW);
+    }
+
+    // Left motor direction control
+    if (leftMotorSpeed < 0) {
+        digitalWrite(leftMotorPin1, LOW);
+        digitalWrite(leftMotorPin2, HIGH);
+    } else if (leftMotorSpeed > 0) {
+        digitalWrite(leftMotorPin1, HIGH);
+        digitalWrite(leftMotorPin2, LOW);
+    } else {
+        digitalWrite(leftMotorPin1, LOW);
+        digitalWrite(leftMotorPin2, LOW);
+    }
+
+    // Control motor speed using PWM
+    analogWrite(enableRightMotor, abs(rightMotorSpeed));
+    analogWrite(enableLeftMotor, abs(leftMotorSpeed));
+}
+
+void stopMotors() {
+    rotateMotors(0, 0);
+}
 
