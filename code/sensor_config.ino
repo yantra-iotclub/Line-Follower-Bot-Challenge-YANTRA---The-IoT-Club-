675caf6e095246d5993d9d334c5c0c93ccca0bc4
🚀 YANTRA - The IoT Club <yantratheiotclub@gmail.com>
2025-03-08 22:07:57 +0530

    Update sensor_config.ino

--- code/sensor_config.ino
+++ code/sensor_config.ino
@@ -1,1 +1,19 @@
+// sensor_config.ino - Line Follower Bot Challenge | YANTRA - The IoT Club
+// This file handles IR sensor configuration and reading.
+
+#define IR_SENSOR_RIGHT 11
+#define IR_SENSOR_LEFT 12
+
+void configureSensors() {
+    pinMode(IR_SENSOR_RIGHT, INPUT);
+    pinMode(IR_SENSOR_LEFT, INPUT);
+}
+
+int readRightSensor() {
+    return digitalRead(IR_SENSOR_RIGHT);
+}
+
+int readLeftSensor() {
+    return digitalRead(IR_SENSOR_LEFT);
+}
 
